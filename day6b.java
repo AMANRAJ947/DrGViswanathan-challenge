41d0590a2be1255c49fe53e6bab76e2c2b5a9351
AMANRAJ947 <amanraj947ar@gmail.com>
2026-09-05 04:25:46 +0530

    day6

---
+++ day6b.java
@@ -0,0 +1,15 @@
+lass Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode curr=head;
+        ListNode prev=null;
+        ListNode temp=null;
+        while(curr != null){
+            temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        head=prev;
+        return head;
+    }
+}    
