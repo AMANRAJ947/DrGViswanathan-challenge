ff3d447cf36c34c037fcf4abf3fd5c67f854e0b0
AMANRAJ947 <amanraj947ar@gmail.com>
2026-09-16 10:26:11 +0530

    day17

---
+++ day17.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++)
+            a[i] = sc.nextInt();
+
+        int left = 0, right = n - 1;
+        int sereja = 0, dima = 0;
+        boolean turn = true;
+
+        while (left <= right) {
+            int card;
+
+            if (a[left] > a[right])
+                card = a[left++];
+            else
+                card = a[right--];
+
+            if (turn)
+                sereja += card;
+            else
+                dima += card;
+
+            turn = !turn;
+        }
+
+        System.out.println(sereja + " " + dima);
+    }
+}
